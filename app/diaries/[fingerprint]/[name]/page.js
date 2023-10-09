43355d0adf6fee5b8965413b201e85aa6c1a8cd3
Michael Eco <michael.gunawan2002@gmail.com>
2023-10-09 15:06:50 +0700

    Update page.js

--- app/diaries/[fingerprint]/[name]/page.js
+++ app/diaries/[fingerprint]/[name]/page.js
@@ -3,7 +3,7 @@ import DiaryTemplate from "./template/diary";
 
 export default async function DiaryPage({ params }) {
   const response = await fetch(
-    `http://127.0.0.1:8000/api/diary/${params.fingerprint}`,
+    `https://janus-cj6m02ocv-ssabrut.vercel.app/api/diary/${params.fingerprint}`,
     { cache: "no-cache" }
   );
 
