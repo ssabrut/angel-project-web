8498a88d8a0f0794bf95827c329ef013743f5062
ssabrut <michael.gunawan2002@gmail.com>
2023-10-09 19:18:54 +0700

    Change api url

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://0.0.0.0:8000/api/:path*",
+        destination: "https://janus-api-ssabrut.vercel.app/api/:path*",
       },
     ];
   },
